45f29f74439b032ce8b80836f1df6e8d42073301
Benjamin Wilberforce <benjaminamonoowilberforce@gmail.com>
2025-08-03 15:18:43 +0000

    Add Inheritance Demo

---
+++ InheritanceDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace InheritanceDemo
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Inheritance & Method Overriding ---");
+
+            Animal generic = new Animal();
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            generic.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
