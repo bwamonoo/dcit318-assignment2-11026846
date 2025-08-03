0c43107d6157c014d472e4f0d065fd6e7c173ca1
Benjamin Wilberforce <benjaminamonoowilberforce@gmail.com>
2025-08-03 15:35:59 +0000

    Add Interface Demo

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Interfaces ---");
+
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
